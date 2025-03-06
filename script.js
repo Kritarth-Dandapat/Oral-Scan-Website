8c36230df6fd8c24f3a8ca6436021ee9d042871b
Kritarth Dandapat <141005022+Kritarth-Dandapat@users.noreply.github.com>
2025-03-06 08:38:08 -0500

    updated pictures of people

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ faqBtns.forEach(button => {
   });
 });
 
+let currentIndex = 0;
+const slides = document.querySelector('.carousel');
+const totalSlides = slides.children.length;
+
+function showSlide(index) {
+  if (index >= totalSlides) currentIndex = 0;
+  else if (index < 0) currentIndex = totalSlides - 1;
+  else currentIndex = index;
+
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+/* Auto Slide Every 3 Seconds */
+let autoSlide = setInterval(() => {
+  nextSlide();
+}, 3000);
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+  resetAutoSlide();
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+  resetAutoSlide();
+}
+
+/* Reset Auto Slide Timer When User Clicks Navigation */
+function resetAutoSlide() {
+  clearInterval(autoSlide);
+  autoSlide = setInterval(() => {
+    nextSlide();
+  }, 3000);
+}
+
